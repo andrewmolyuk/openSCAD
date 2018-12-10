84e025d172bea256cb303bc6bc42873b7bb95740
Andrew Molyuk <andrew@molyuk.com>
2018-12-10 23:36:49 +0200

    ABS build removed.

--- mc2-spacer/build.sh
+++ mc2-spacer/build.sh
@@ -7,4 +7,3 @@ mkdir -p output
 /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD -o output/mc2-spacer.png --imgsize=320,240 --colorscheme=Tomorrow mc2-spacer.scad
 
 /Applications/Slic3r.app/Contents/MacOS/Slic3r --no-gui --load slic3r-pla.ini --output output/mc2-spacer-pla.gcode output/mc2-spacer.stl
-/Applications/Slic3r.app/Contents/MacOS/Slic3r --no-gui --load slic3r-abs.ini --output output/mc2-spacer-abs.gcode output/mc2-spacer.stl
